5f22b4990d8792bcaae709bdd85037a637a5ef07
HernandezM-dev <HernandezM.dev@gmail.com>
2020-07-27 15:28:44 -0400

    installed react-router-dom, imported browser router and wrapped app.js

--- water_plants/src/index.js
+++ water_plants/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import './styles/index.css';
 
 ReactDOM.render(
-    <App />,
+    <Router><App /></Router>,
   document.getElementById('root')
 );
