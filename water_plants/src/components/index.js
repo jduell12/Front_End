dcdb4fa376e97a1dd5ccd026b964711154bfcc31
Jessica <duellje@oregonstate.edu>
2020-07-27 15:48:26 -0400

    added EditUser component

--- water_plants/src/components/index.js
+++ water_plants/src/components/index.js
@@ -1,5 +1,6 @@
 import PrivateRoute from './PrivateRoute';
 import AddPlant from './AddPlant';
 import EditPlant from './EditPlant';
+import EditUser from './EditUser';
 
-export {PrivateRoute, AddPlant, EditPlant};
+export {PrivateRoute, AddPlant, EditPlant, EditUser};
