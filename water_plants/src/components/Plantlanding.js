7b6cc1846b1344882f1809c6f64d8ce96318d3bd
Jessica Duell <duellje@oregonstate.edu>
2020-07-27 21:57:30 -0400

    Create Plantlanding.js

---
+++ water_plants/src/components/Plantlanding.js
@@ -0,0 +1,23 @@
+//Hernandez
+import React from 'react'
+import Plant from './Plant'
+
+
+// export default function Plantlanding(props){
+
+//     // const {plants} = props
+
+//     return(
+//         // <div className='plant-page'>
+//         //     <h2> Your Plants!</h2>
+//         //     <div className='card-holder'>
+//         //         {
+//         //             plants.map( plant =>{
+//         //                 <Plant plant={plant}/>
+//         //             })
+//         //         }
+//         //     </div>
+            
+//         // </div>
+//     // )
+// }
