7768c1024314a31bdf3b992cc018ce2b32515034
Jessica Duell <duellje@oregonstate.edu>
2020-07-27 21:56:55 -0400

    Create Plant.js

---
+++ water_plants/src/components/Plant.js
@@ -0,0 +1,25 @@
+//Hernandez
+import React from 'react';
+
+export default function Plant(props){
+    // ,may need to add prop for img from API
+    const {plant} = props
+
+    return(
+        <div className='plant-card'>
+            <h3>{plant.name}</h3>
+            {/* possibly img from API based on species */}
+            <div className='plant-details'>
+                <p>{plant.id}</p>
+                <p>{plant.frequency}</p>
+                <p>{plant.species}</p>
+                <p>{plant.lwd}</p>
+            </div>
+            <div className='buttons'>
+                <button>Edit</button>
+                <button>Delete</button>
+            </div>
+        </div>
+    )
+        
+}
