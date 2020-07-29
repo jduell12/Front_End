455506187d23ee973a5cb7eebd0afb1d1b96d240
megawatts68 <andrew.f.watts@gmail.com>
2020-07-30 02:58:13 +0700

    Started styling.

--- water_plants/src/components/UserProfile.js
+++ water_plants/src/components/UserProfile.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { axiosWithAuth } from '../utils/axiosWithAuth'
+import { ContainerDiv, CardDiv } from '../styles/userprofile-styles'
+import styled from 'styled-components'
+
 
 const initialUserValue = {
     username: '',
@@ -16,31 +19,31 @@ export default function UserProfile() {
 
     useEffect(() => {
         axiosWithAuth()
-        .get('/myinfo')
-        .then((resp) => {
-            console.log('RESP', resp.data)
-            setUser(resp.data)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+            .get('/myinfo')
+            .then((resp) => {
+                console.log('RESP', resp.data)
+                setUser(resp.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }, [])
-    
+
 
     return (
-        <div>
-            <div>
+        <ContainerDiv>
+            <CardDiv>
                 <h1>Profile</h1>
                 <h2>Username: {user.username}</h2>
                 <h2>First Name: {user.firstname}</h2>
                 <h2>Last Name: {user.lastname}</h2>
                 <p>email: {user.primaryemail}</p>
                 <p>phone: {user.phone}</p>
-            </div>
-            <div>
-                <button>Edit User Profile</button>
-                <button>Delete User Profile</button>
-            </div>
-        </div>
+                <div>
+                    <button>Edit User Profile</button>
+                    <button>Delete User Profile</button>
+                </div>
+            </CardDiv>
+        </ContainerDiv>
     )
 }
---
+++ water_plants/src/styles/userprofile-styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const ContainerDiv = styled.div`
+    background-color: seagreen;
+    padding: 10%;
+    
+`
+export const CardDiv = styled.div`
+    width: 50%;
+    background-color: teal;
+    padding: 5% 10%;
+    margin: 10%;
+    border-radius: 15%;
+    box-shadow: 5px 10px;
+
+    h1 {
+        text-align: center;
+    }
+
+`
+export default ContainerDiv;
