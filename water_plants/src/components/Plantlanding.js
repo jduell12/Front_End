7053e4c9c95d5fc37248bab6f1e286f16c7937b1
Jessica <duellje@oregonstate.edu>
2020-07-28 22:00:55 -0400

    set up context and linking

--- water_plants/src/components/Plantlanding.js
+++ water_plants/src/components/Plantlanding.js
@@ -1,47 +1,38 @@
 //Hernandez
-import React, {useState, useEffect} from "react";
+import React, {useState, useContext} from "react";
 import {Link} from 'react-router-dom';
 import Plant from './Plant'
 import {AddPlant} from './'
-import {axiosWithAuth} from '../utils/axiosWithAuth'
-
 
 //context for user
 import {UserContext} from '../context/UserContext'
 
-export default function Plantlanding(props){
+export default function Plantlanding(){
 
-    const [plants, setPlants] = useState([]);
+    const {userInfo} = useContext(UserContext);
+    const {plants} = userInfo;
 
-    useEffect(() => {
-        axiosWithAuth()
-            .get('https://watermyplantsdatabase.herokuapp.com/myinfo') 
-            .then(res => {
-                setPlants(res.data.plants)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }, []); 
-
-   
+    const [plantList, setPlants] = useState(userInfo[plants]);
 
     return(
         <div className='plant-page'>
             <h2> Your Plants!</h2>
-            
             <Link to="/private/addplant"><button>Add Plant </button></Link>
+            {!plantList ? <span></span> : 
+                (
             <div>
                 {
-                    !plants.length ? (<span></span>) : (
+                    !plantList.length ? (<span></span>) : (
                         <div className='card-holder'>
                             {
-                             plants.map(plant => <Plant plant={plant}/>)
+                             plantList.map(plant => <Plant plant={plant}/>)
                             }
                         </div>
                     )
                 }
             </div>
+                )
+            }
         </div>
     )
 }
