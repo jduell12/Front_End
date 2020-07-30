8b60ab7150a0513931fb22bc115d0d3c864c7dcc
megawatts68 <andrew.f.watts@gmail.com>
2020-07-31 03:01:29 +0700

    Styling...

--- water_plants/src/styles/userprofile-styles.js
+++ water_plants/src/styles/userprofile-styles.js
@@ -1,22 +1,34 @@
-import styled from 'styled-components'
+// import styled from 'styled-components'
+import { makeStyles } from '@material-ui/core/styles'
 
-export const ContainerDiv = styled.div`
-    background: linear-gradient(to right, seagreen 0%, green 100%);
-    padding: 10%;
-    
-`
-export const CardDiv = styled.div`
-    width: 30%;
-    background-color: teal;
-    padding: 5% 10%;
-    margin: 10%;
-    border-radius: 15%;
-    box-shadow:10px 10px 5px 0px rgba(0,0,0,0.75);
-    color: white;
-
-    h1 {
-        text-align: center;
+const useStyles = makeStyles((theme) => ({
+    cardDiv: {
+        width: '30%',
+        backgroundColor: '#3B945E',
+        padding: theme.spacing(3, 6),
+        margin: '10% auto',
+        boxShadow:'10px 10px 5px 0px rgba(0,0,0,0.75)',
+        color: 'white',
     }
+}))
 
-`
-export default ContainerDiv;
+export default useStyles
+
+// export const ContainerDiv = styled.div`
+//     padding: 10%;
+    
+// `
+// export const CardDiv = styled.div`
+//     width: 30%;
+//     background-color: teal;
+//     padding: 5% 10%;
+//     margin: auto;
+//     border-radius: 15%;
+//     box-shadow:10px 10px 5px 0px rgba(0,0,0,0.75);
+//     color: white;
+
+//     h1 {
+//         text-align: center;
+//     }
+
+// `
