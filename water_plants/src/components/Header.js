2ca3aec5930100326c56e5b34e44744bf5626cdf
Jessica <duellje@oregonstate.edu>
2020-07-30 10:02:22 -0400

    styled add plant

--- water_plants/src/components/Header.js
+++ water_plants/src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState} from 'react';
-import { Switch, Link, useHistory} from 'react-router-dom'
+import { Link, useHistory} from 'react-router-dom'
 
 
 export default function Header(props){
